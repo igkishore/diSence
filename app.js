0345a76766bd874c339299776d745f316d2f0e9b
igkishore <gowthamkishoreindukuri@gmail.com>
2021-01-28 20:30:31 +0530

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
-var port = (process.env.PORT || 8080);
+var port = (process.env.PORT || 3000);
 app.listen(port);
 
 app.set('view engine','ejs');
